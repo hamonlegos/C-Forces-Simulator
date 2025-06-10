c821cf7fb87b0435baaf7d9fa894082b28c0b518
alho caralho <dann.ru@proton.me>
2025-06-10 03:09:55 +0100

    started ball physics but couldnt get iostream to work LMAO

--- main.c
+++ main.c
@@ -2,18 +2,23 @@
 
 int main(void) {
     const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenHeight = 800;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    InitWindow(screenWidth, screenHeight, "ball gravity sim");
+
+    Vector2 ballpos = {400.f, 400.f};
+    int mass = 40;
+    Vector2 tforces = {};
+    float time;
 
     SetTargetFPS(60);
     while (!WindowShouldClose()) {
+
+        time += 1 / 60;
+
         BeginDrawing();
-
-            ClearBackground(RAYWHITE);
-
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-
+            DrawCircleV(ballpos, 35.f, GREEN);
+            ClearBackground(DARKGRAY);
         EndDrawing();
     }
     CloseWindow();
